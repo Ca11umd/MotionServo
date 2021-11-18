03b0c3f591a7bea186eee001cfc02b27a017885c
Ca11umd <69772718+Ca11umd@users.noreply.github.com>
2021-11-18 20:46:37 +0000

    yes

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,4 @@
 #include <Arduino.h>
 #include <Servo.h>
 
-int ledPin = 13;
-int pirPin = 7;
-int pirValue;
-
-Servo MotionServo;
-
-
-void setup() {
-  pinMode(ledPin, OUTPUT);
-  pinMode(pirPin, INPUT);
-  digitalWrite(ledPin, LOW);
-  MotionServo.attach(6);
-  MotionServo.write(90);
-
-}
-
-void loop() {
-  pirValue = digitalRead(pirPin);
-  digitalWrite(ledPin, pirValue);
-  
-  if (pirValue == HIGH) {
-    MotionServo.write(180);
-  }
-  else {
-    MotionServo.write(90);
-  }
-  
-}
 
