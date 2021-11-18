dcdc94bd2e09d1f88a6a9f56986e371c8d3bc8d7
Ca11umd <69772718+Ca11umd@users.noreply.github.com>
2021-11-18 20:21:22 +0000

    Working

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,32 @@
 #include <Arduino.h>
-
 #include <Servo.h>
-Servo myservo;
 
-void setup(){
-  myservo.attach(9);
-  myservo.write(90);// move servos to center position -> 90°
-  Serial.begin(9600);
-} 
-void loop(){
-  Serial.write("Starting\n");
-  myservo.write(90);// move servos to center position -> 90°
-  delay(1000);
-  myservo.write(30);// move servos to center position -> 60°
-  delay(1000);
-  myservo.write(90);// move servos to center position -> 90°
-  delay(1000);
-  myservo.write(150);// move servos to center position -> 120°
-  delay(1000);
+int ledPin = 13;
+int pirPin = 7;
+int pirValue;
+
+Servo MotionServo;
+
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(pirPin, INPUT);
+  digitalWrite(ledPin, LOW);
+  MotionServo.attach(6);
+  MotionServo.write(90);
+
 }
+
+void loop() {
+  pirValue = digitalRead(pirPin);
+  digitalWrite(ledPin, pirValue);
+  
+  if (pirValue == HIGH) {
+    MotionServo.write(180);
+  }
+  else {
+    MotionServo.write(90);
+  }
+  
+}
+
